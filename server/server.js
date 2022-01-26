1e8c2dd06e1a31992413de46847314e6f0cfa245
aek20 <66400022+aek20@users.noreply.github.com>
2022-01-26 21:28:27 +0300

    26/1

--- server/server.js
+++ server/server.js
@@ -2,8 +2,9 @@ import express from 'express'
 import add from './src/DB/database.js'
 import deleteCom from './src/DB/delete.js'
 import show from './src/DB/showComics.js'
+import update from './src/DB/update.js'
 const app = express()
-const port = 3959
+const port = 3929
 
 
 app.use(express.json())
@@ -25,5 +26,9 @@ app.get('/show', (req, res) => {
   res.send('show data')
 })
 
+app.post('/update', function (req, res) {
+  update()
+  res.send('update')
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))

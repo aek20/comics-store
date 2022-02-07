883ae46b72cc60d130ab0669b5ff1cbb03bbe996
aek20 <66400022+aek20@users.noreply.github.com>
2022-02-07 23:10:24 +0300

    2/7

--- my-app/src/components/addComics/Add.js
+++ my-app/src/components/addComics/Add.js
@@ -1,10 +1,24 @@
 import "./add.css"
 import React from 'react';
 import {Form,Col,Row} from 'react-bootstrap'
-const Add = () => {
+const Add =  async() => {
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log('You clicked submit.');
+sending()
+    }
+    let sending = await fetch("https://httpbin.org/post", {
+        method: "POST",
+        body: JSON.stringify({
+            name: "name",
+            email: "email",
+            mobileNumber: "mobileNumber",
+        }),
+    });
+
     return (
         <div>
-            <Form>
+            <Form onSubmit={handleSubmit}>
        <Form.Group as={Row} className="mb-3" controlId="plaintext">
                     <Form.Label column sm="2">
                         Company
@@ -35,11 +49,17 @@ const Add = () => {
                     </Form.Label>
                     <Col sm="10">
                         <Form.Control type="plaintext" placeholder="enter company name" />
+
                     </Col>
+         
                 </Form.Group>
+                <button>ADD</button>
             </Form>
         </div>
     );
 }
+
+
+
 
 export default Add;
